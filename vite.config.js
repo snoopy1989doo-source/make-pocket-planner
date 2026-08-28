c9e7c63b2a715f41b8206114a6d64e4aa25ab939
snoopy1989doo-source <snoopy1989doo@gmail.com>
2026-08-28 23:55:27 +0700

    Fix white screen by using relative base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/make-pocket-planner/',
+  // Use relative base path so that it runs successfully inside Capacitor (Android)
+  // as well as on GitHub Pages subfolder.
+  base: './',
   build: {
     outDir: 'www',
     emptyOutDir: true
